c6bddd36e35bb63517734cb2d311c693f95d9fad
Nicholas Shephard <shephardn@gmail.com>
2021-11-01 10:52:26 -0500

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Nick';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = `Welcome, ${currentUser[0]}!`;
+//const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
